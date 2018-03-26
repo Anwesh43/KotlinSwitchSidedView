00e2701d8527f4d85be13904e9605d403b0a6ad1
Anwesh43 <anweshthecool0@gmail.com>
2018-03-27 01:02:34 +0530

    adding SidedSwitchView class

--- switchsidedview/src/main/java/ui/anwesome/com/switchsidedview/SidedSwitchView.kt
+++ switchsidedview/src/main/java/ui/anwesome/com/switchsidedview/SidedSwitchView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.switchsidedview
 /**
  * Created by anweshmishra on 27/03/18.
  */
+import android.graphics.*
+import android.content.*
+import android.view.*
+class SidedSwitchView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
